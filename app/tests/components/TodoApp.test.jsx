2d795a1f3fa37aa85fdf0f3472c4f27a80004727
Paoonline <paoonline@windowslive.com>
2018-10-23 16:31:37 +0700

    New test Todo

---
+++ app/tests/components/TodoApp.test.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import expect from 'expect'
+import $  from 'jQuery'
+import TestUtils from 'react-addons-test-utils'
+import TodoApp from 'TodoApp'
+
+describe('TodoApp', ()=>{
+    it('should exist', ()=>{
+        expect(TodoApp).toExist();
+    })
+})
